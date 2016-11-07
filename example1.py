d43437402297b73c88fbc1a85daab8d5a1bf4439
Mario Geiger <geiger.mario@gmail.com>
2016-11-07 19:30:16 +0100

    improve python script

--- example1.py
+++ example1.py
@@ -1,27 +1,32 @@
+# -*- coding: utf-8 -*-
+# régression linéaire
 import metropolis
 import numpy as np
-
-# régression linéaire 
-
+import matplotlib.pyplot as plt
 
 # création du modèle :
 def model(p, x):
-	# p[0] is used internaly
-	return p[1] * x + p[2]
+	return p[0] * x + p[1]
 
 # génération aléatoire des données :
-x = np.linspace(-5, 15, 120)
-y = 4.5 * x + 12 + np.random.randn(len(x))
+x = np.linspace(-5, 15, 30)
+y = 4.5 * x + 12 + 2 * np.random.randn(len(x))
 
 # fit :
 p = metropolis.metropolis(model, x, y, [5, 10], [0.5, 1])
 
-
 # moyennes et écarts types :
-a  = np.mean(p[:,1], axis=0)
-da = np.std( p[:,1], axis=0)
-b  = np.mean(p[:,2], axis=0)
-db = np.std( p[:,2], axis=0)
+a  = np.mean(p[:,0], axis=0)
+da = np.std( p[:,0], axis=0)
+b  = np.mean(p[:,1], axis=0)
+db = np.std( p[:,1], axis=0)
 
 print "pente = {} +- {}".format(a, da)
-print "ordonnee a l'origine = {} +- {}".format(b, db)
+print "ordonnée à l'origine = {} +- {}".format(b, db)
+
+# plots
+plt.figure()
+plt.plot(x, y, 'o')
+x_ = np.linspace(-6, 16, 1000)
+plt.plot(x_, model(np.mean(p, axis=0), x_), '-')
+plt.show()
--- metropolis.py
+++ metropolis.py
@@ -3,16 +3,17 @@ import emcee
 from math import log
 
 def metropolis(model, x, y, p_init, p_var, Niter=1000, Ninit=300, Nwalkers=10):
+	
 	def lnprob(p, x, y, model):
 		# prior (sigma is nonnegative)
 		if p[0] < 0:
-		    return -np.inf
+			return -np.inf
 		# log of likelihood
 		return - 0.5 * x.shape[0] * log(abs(p[0])) \
-		       - np.sum(np.power(y - model(p, x), 2)) / (2 * p[0] ** 2)
+		       - np.sum(np.power(y - model(p[1:], x), 2)) / (2 * p[0] ** 2)
 	
 	# choose initial parameters for each walker
-	p0 = [np.append(np.random.rand(1) * np.std(y), p_init + np.random.rand(len(p_var)) * p_var) for i in range(Nwalkers)]
+	p0 = [np.append(np.random.rand(1) * np.std(y), p_init + np.random.randn(len(p_var)) * p_var) for i in range(Nwalkers)]
 	
 	# create emcee main object
 	sampler = emcee.EnsembleSampler(Nwalkers, len(p_init) + 1, lnprob, args=[x, y, model])
@@ -25,4 +26,4 @@ def metropolis(model, x, y, p_init, p_var, Niter=1000, Ninit=300, Nwalkers=10):
 	sampler.run_mcmc(pos, Niter)
 	
 	
-	return sampler.flatchain
+	return sampler.flatchain[:, 1:]
